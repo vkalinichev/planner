3fd5be1b77009c0717cd6f86b416152885d460db
Vladimir Kalinichev <wrumly@gmail.com>
2016-10-23 00:12:32 +0300

    Test for delete task

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,6 +1,7 @@
 import * as actions from './actions';
 
 describe( 'actions', () => {
+
     it('should create an action to update a task', () => {
         const task = {
             title: "new title",
@@ -11,5 +12,14 @@ describe( 'actions', () => {
             data: task
         };
         expect( actions.updateTask( task ) ).toEqual( expectedAction )
+    });
+
+    it('should create an action to delete a task', () => {
+        const taskId = 17;
+        const expectedAction = {
+            type: "DELETE_TASK",
+            data: taskId
+        };
+        expect( actions.deleteTask( taskId ) ).toEqual( expectedAction )
     })
 });
