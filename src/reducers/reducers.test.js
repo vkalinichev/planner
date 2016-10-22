220047cf59fe26ceb464412731a81e8e504f6a6e
Vladimir Kalinichev <wrumly@gmail.com>
2016-10-23 00:26:04 +0300

    Tests for add task reducer

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,31 @@
+import * as reducers from './reducers';
+
+describe( 'tasks reducer', () => {
+
+    it('should return the initial state', () => {
+        expect(
+            reducers.tasks(undefined, {})
+        ).toEqual([])
+    });
+
+    it('should handle ADD_TASK', () => {
+        const task0 = {
+            id: 0,
+            title: 'Use Redux',
+            text: 'subj'
+        };
+        const task1 = {
+            id: 1,
+            title: 'Run the tests',
+            text: 'subj'
+        };
+        const action = {
+            type: 'ADD_TASK',
+            data: task1
+        };
+        expect( reducers.tasks([], action ) ).toEqual( [ task1 ] );
+
+        expect( reducers.tasks([ task0 ], action ) ).toEqual( [ task0, task1 ] );
+    })
+
+});
