02a9822ddd67a6b75f30f813083e474001ba2371
Vladimir Kalinichev <wrumly@gmail.com>
2016-11-07 23:51:21 +0300

    Koa-passport boilerplate

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
+const session = require( 'koa-generic-session' )
+const bodyParser = require( 'koa-bodyparser' )
 const compress = require( 'koa-compress' )
+const passport = require( 'koa-passport' )
+const Router = require( 'koa-router' )
 const logger = require( 'koa-logger' )
 const serve = require( 'koa-static' )
-const route = require( 'koa-route' )
 const koa = require( 'koa' )
 const app = module.exports = koa()
 const tasks = require( './controllers/tasks' )
@@ -10,19 +13,74 @@ const { API_SERVER: { HOST, PORT }, PATHS: { BUILD } } = require( '../config' )
 
 app.use( logger() )
 
-app.use( route.get( '/projects/', projects.all ) )
-app.use( route.get( '/projects/:id', projects.fetch ) )
-app.use( route.post( '/projects/', projects.add ) )
-app.use( route.put( '/projects/:id', projects.modify ) )
-app.use( route.delete( '/projects/:id', projects.remove ) )
+app.proxy = true
 
-app.use( route.get( '/tasks/', tasks.all ) )
-app.use( route.get( '/tasks/:id', tasks.fetch ) )
-app.use( route.post( '/tasks/', tasks.add ) )
-app.use( route.put( '/tasks/:id', tasks.modify ) )
-app.use( route.delete( '/tasks/:id', tasks.remove ) )
+app.keys = [ 'your-session-secret' ]
+app.use( session() )
+
+app.use( bodyParser() )
+
+require( './auth/auth' )
+app.use( passport.initialize() )
+app.use( passport.session() )
+
+var pub = new Router()
+
+pub.post( '/auth', function* ( next ) {
+    var ctx = this
+    yield passport.authenticate( 'local', function* ( err, user, info ) {
+        if ( err ) throw err
+        if ( user === false ) {
+            ctx.status = 401
+            ctx.body = { success: false }
+        } else {
+            yield ctx.login( user )
+            ctx.body = { success: true }
+        }
+    } ).call( this, next )
+} )
+
+pub.post( '/login', passport.authenticate( 'local', {
+    successRedirect: '/',
+    failureRedirect: '/'
+} ) )
+
+pub.get( '/logout', function* ( next ) {
+    this.logout()
+    this.redirect( '/' )
+} )
+
+app.use( pub.middleware() )
 
 app.use( serve( BUILD ) )
+
+// Require authentication for now
+app.use( function* ( next ) {
+    if ( this.isAuthenticated() ) {
+        yield next
+    } else {
+        this.redirect( '/non-authorized' )
+    }
+} )
+
+var api = new Router()
+
+api
+    .get( '/app', () => console.log( 'authorized' ) )
+    .get( '/projects/', projects.all )
+    .get( '/projects/:id', projects.fetch )
+    .post( '/projects/', projects.add )
+    .put( '/projects/:id', projects.modify )
+    .delete( '/projects/:id', projects.remove )
+
+api
+    .get( '/tasks/', tasks.all )
+    .get( '/tasks/:id', tasks.fetch )
+    .post( '/tasks/', tasks.add )
+    .put( '/tasks/:id', tasks.modify )
+    .delete( '/tasks/:id', tasks.remove )
+
+app.use( api.middleware() )
 app.use( compress() )
 
 app.listen( PORT, ()=>
